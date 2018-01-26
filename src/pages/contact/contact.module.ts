c62e2c76d03a7fb00f1f9d87f07255baad85bec6
gonghao <569568313@qq.com>
2018-01-26 16:04:13 +0800

    修改部分代码

--- src/pages/contact/contact.module.ts
+++ src/pages/contact/contact.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core'
 import { IonicPageModule } from 'ionic-angular'
 import { ContactPage } from './contact'
+import { IonProductsComponentModule } from '../../components/ion-products/ion-products.module'
 
 @NgModule({
   declarations: [
@@ -8,6 +9,7 @@ import { ContactPage } from './contact'
   ],
   imports: [
     IonicPageModule.forChild(ContactPage),
+    IonProductsComponentModule
   ]
 })
 export class ContactPageModule { }
